82edb05adc7139b68c407fc565a2a5ff828d4fe5
runner <runner@Mac-1646147279188.local>
2022-03-01 15:31:36 +0000

    release/0.9.0

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskSDKFayeClient",
                  url: "https://github.com/zendesk/sdk_faye_client_ios",
-                 from: "0.4.3"),
+                 from: "0.5.0"),
         .package(name: "ZendeskSDKHTTPClient",
                  url: "https://github.com/zendesk/sdk_http_client_ios",
-                 from: "0.5.2"),
+                 from: "0.6.0"),
         .package(name: "ZendeskSDKStorage",
                  url: "https://github.com/zendesk/sdk_storage_ios",
-                 from: "0.4.3")
+                 from: "0.4.4")
     ],
     targets: [
         .binaryTarget(
