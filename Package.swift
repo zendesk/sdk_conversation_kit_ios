4d5636833b155b7e198a2289e5b3c054df52003e
runner <runner@zendesk.com>
2024-04-24 15:22:51 +0000

    [RELEASE] New version 4.0.1

--- Package.swift
+++ Package.swift
@@ -17,22 +17,22 @@ let package = Package(
         .package(
             name: "ZendeskSDKFayeClient",
             url: "https://github.com/zendesk/sdk_faye_client_ios",
-            from: "1.8.0"
+            from: "1.8.1"
         ),
         .package(
             name: "ZendeskSDKHTTPClient",
             url: "https://github.com/zendesk/sdk_http_client_ios",
-            from: "0.15.0"
+            from: "0.15.1"
         ),
         .package(
             name: "ZendeskSDKStorage",
             url: "https://github.com/zendesk/sdk_storage_ios",
-            from: "1.0.0"
+            from: "1.0.1"
         ),
         .package(
             name: "ZendeskSDKCoreUtilities",
             url: "https://github.com/zendesk/sdk_core_utilities_ios",
-            from: "2.2.0"
+            from: "2.2.1"
         )
     ],
     targets: [
